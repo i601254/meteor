c8afef9795efe480c4216397ca482268ff4f70f1
i601254 <photofxpros@gmail.com>
2016-09-16 16:07:35 -0400

    added geocode data

--- server/main.js
+++ server/main.js
@@ -8,6 +8,19 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
+tj = Npm.require('togeojson');
+fs = Npm.require('fs');
+    // node doesn't have xml parsing or a dom. use jsdom
+jsdom = Npm.require('jsdom').jsdom;
+
+var geodata = Assets.getText('office_locations_tracks_ogrgeojson.geojson');
+var gpx = jsdom(fs.readFileSync(geodata));
+
+//var geojsonFeature = tj.gpx(gpx);
+//console.log(geojsonFeature);
+
+console.log(geodata);
+
 var curl = require('curlrequest');
 
 Meteor.methods({
@@ -20,6 +33,10 @@ Meteor.methods({
   'plotMap': function(options){ 
      var future = new Future();
      var urlRequest = Meteor.wrapAsync(curl.request(options));
+  //  var geodata = Assets.getText('office_locations.gpx');
+  //  var gpx = jsdom(fs.readFileSync(geodata));
+  //  var geojsonFeature = tj.kml(gpx);
+
        urlRequest(function(err,resp) {
          if (err) {
            console.log(err);
@@ -41,15 +58,21 @@ Meteor.methods({
               "coordinates": [ a[property].longitude, a[property].latitude ]
             }
         };
-            Locations.insert(geojsonFeature);
-//            console.log(geojsonFeature);
-//            console.log(Object.keys(geojsonFeature).length);
-//            return future["return"](geojsonFeature);
+           Locations.insert(geojsonFeature);
+////            console.log(geojsonFeature);
+////            Meteor.publish('jobs', function(){
+////              return Locations.find();
+////            });
+////            console.log(Object.keys(geojsonFeature).length);
+////            return future["return"](geojsonFeature);
         }
       }
-     }
-    });
+     }  //close 'else'
+    });  //close 'urlRequest(function)'
     return future.wait();
+    Meteor.publish('jobs', function(){
+      return Locations.find();
+    });
   }
 });
 
