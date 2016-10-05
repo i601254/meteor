5c793293d07f18d0456f99541012109a0ff52107
i601254 <photofxpros@gmail.com>
2016-10-05 16:08:35 -0400

    changes

--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import { Locations } from '../imports/api/locations.js';
-//import { HTTP } from 'meteor/http'
+//import { Locations } from '../imports/api/locations.js';
+//import { Zones } from '../imports/api/locations.js';
+
+//Zones = new Mongo.Collection('zones');
 
 Future = Npm.require('fibers/future');
 
@@ -8,71 +10,83 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
-tj = Npm.require('togeojson');
-fs = Npm.require('fs');
-    // node doesn't have xml parsing or a dom. use jsdom
-jsdom = Npm.require('jsdom').jsdom;
-
-var geodata = Assets.getText('office_locations_tracks_ogrgeojson.geojson');
-var gpx = jsdom(fs.readFileSync(geodata));
-
-//var geojsonFeature = tj.gpx(gpx);
-//console.log(geojsonFeature);
-
-console.log(geodata);
-
-var curl = require('curlrequest');
-
 Meteor.methods({
   'getSessionId': function(){
     return this.connection.id;
   }
 });
 
+var curl = require('curlrequest');
+
 Meteor.methods({
-  'plotMap': function(options){ 
-     var future = new Future();
-     var urlRequest = Meteor.wrapAsync(curl.request(options));
+  'plotMap': function(options){
+    var future = new Future();    
+    var urlRequest = Meteor.wrapAsync(curl.request(options));
+    
+    urlRequest(function(err,resp) {
+      if (err) {
+        console.log(err);
+      }else{
+      var a = JSON.parse(resp);
+      //console.log(resp);
+      future["return"](a);
+      }
+    });
+      return future.wait();
+  }
+});
+
+  //  var future = new Future();
+  //  var urlRequest = Meteor.wrapAsync(curl.request(options));
   //  var geodata = Assets.getText('office_locations.gpx');
   //  var gpx = jsdom(fs.readFileSync(geodata));
   //  var geojsonFeature = tj.kml(gpx);
+  
+//      zones = Zones.find( { "properties.name" : "TKIII8"});
+//      console.log(zones);
+//      return future["return"](zones);
 
-       urlRequest(function(err,resp) {
-         if (err) {
-           console.log(err);
-         }else{
-           var a = JSON.parse(resp);
 
-      let geojsonFeature = {};
-      for (var property in a){
-        if (a.hasOwnProperty(property)) {
-          geojsonFeature = {
-            "type": "Feature",
-            "properties": {
-              "name": property,
-              "fips_county": a[property].fips_county,
-              "popupContent": property
-            },
-            "geometry": {
-              "type": "Point",
-              "coordinates": [ a[property].longitude, a[property].latitude ]
-            }
-        };
-           Locations.insert(geojsonFeature);
+ 
+//      var geodata = Assets.getText('office_locations_tracks_ogrgeojson.geojson');
+//      var gpx = Meteor.wrapAsync(jsdom(fs.readFileSync(geodata)));
+
+//       urlRequest(function(err,resp) {
+//         if (err) {
+ //          console.log(err);
+//         }else{
+//           var a = JSON.parse(resp);
+
+//      let geojsonFeature = {};
+//      for (var property in a){
+//       if (a.hasOwnProperty(property)) {
+//          geojsonFeature = {
+//            "type": "Feature",
+//            "properties": {
+//              "name": property,
+//              "fips_county": a[property].fips_county,
+//              "popupContent": property
+//            },
+//            "geometry": {
+//              "type": "Point",
+//              "coordinates": [ a[property].longitude, a[property].latitude ]
+//            }
+//        };
+//           Locations.insert(geojsonFeature);
 ////            console.log(geojsonFeature);
 ////            Meteor.publish('jobs', function(){
 ////              return Locations.find();
 ////            });
 ////            console.log(Object.keys(geojsonFeature).length);
-////            return future["return"](geojsonFeature);
-        }
-      }
-     }  //close 'else'
-    });  //close 'urlRequest(function)'
-    return future.wait();
-    Meteor.publish('jobs', function(){
-      return Locations.find();
-    });
-  }
-});
+//            return future["return"](gpx);
+//        }
+//      }
+//     }  //close 'else'
+//    });  //close 'urlRequest(function)'
+//    return future.wait();
+//    Meteor.publish('jobs', function(){
+//      return Locations.find();
+//    });
+//  }
+//});
 
